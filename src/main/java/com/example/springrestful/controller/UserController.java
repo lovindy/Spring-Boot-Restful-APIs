e0f9b92a8724b556945326f4c2a849666e3cd8cf
Seang Makaramarady <seangdy085@gmail.com>
2024-11-21 17:46:03 +0700

    feature: implement user authentication

---
+++ src/main/java/com/example/springrestful/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.example.springrestful.controller;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("api/users")
+public class UserController {
+
+
+
+}
