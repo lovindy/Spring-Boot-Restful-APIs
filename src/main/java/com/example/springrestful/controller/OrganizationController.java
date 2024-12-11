681f8bf83b804615e9f2cab17f4b3341d8838583
lovindy <seangdy085@gmail.com>
2024-12-10 19:22:43 -0800

    feaure: allow origin cors

--- src/main/java/com/example/springrestful/controller/OrganizationController.java
+++ src/main/java/com/example/springrestful/controller/OrganizationController.java
@@ -14,6 +14,7 @@ import java.nio.file.AccessDeniedException;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/v1/organizations")
 @RequiredArgsConstructor
 public class OrganizationController {
